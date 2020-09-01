f33221e06873fe573c5191366b1945817a352f29
lujan20 <70240955+lujan20@users.noreply.github.com>
2020-09-01 00:54:17 -0500

    Add files via upload

---
+++ proyectoif.java
@@ -0,0 +1,14 @@
+
+public class proyectoif {
+public static void main(String args[]) {
+String genero = "M";
+if(genero=="F") {
+	System.out.println("MUJER");
+}else {
+	System.out.println("Hombre");
+}	
+	
+	}
+	
+}
+
